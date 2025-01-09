b5d86859108a1d3dc93ac99cbbdcf674b9bafc05
rider <ridebus82@nate.com>
2025-01-09 12:14:37 +0900

    250109 asdfljgogogogo

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { sql_con } from '../back-lib/db.js'
+// import { sql_con } from '../back-lib/db.js'
 import { getQueryStr } from '../back-lib/lib.js';
 import moment from "moment-timezone";
 import bcrypt from "bcrypt";
@@ -7,17 +7,17 @@ const mainRouter = express.Router();
 
 
 
-mainRouter.get('/load_footer', async (req, res) => {
-    let footerData = {};
-    try {
-        const getFooterDataQuery = "SELECT * FROM form_status"
-        const [footerDataRows] = await sql_con.promise().query(getFooterDataQuery);
-        footerData = footerDataRows[0]
-    } catch (error) {
+// mainRouter.get('/load_footer', async (req, res) => {
+//     let footerData = {};
+//     try {
+//         const getFooterDataQuery = "SELECT * FROM form_status"
+//         const [footerDataRows] = await sql_con.promise().query(getFooterDataQuery);
+//         footerData = footerDataRows[0]
+//     } catch (error) {
 
-    }
-    res.json({ footerData })
-})
+//     }
+//     res.json({ footerData })
+// })
 
 
 export { mainRouter }
